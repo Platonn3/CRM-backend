a32eab415bcdcccd7c41c1370bf489f667bcbecf
Platonn3 <platon.kurbatov87@gmail.com>
2025-11-30 22:33:36 +0500

    add new routes for admin

--- app/api/v1/admin.py
+++ app/api/v1/admin.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.models import Master as MasterModel, Service as ServiceModel, Appointment as AppointmentModel
+from app.database.models import Master as MasterModel, Service as ServiceModel, Appointment as AppointmentModel, Client as ClientModel
+from app.schemas.appointment import AppointmentCreate, AppointmentResponse
 
-from app.schemas.master import MasterResponse, MasterCreate
+from app.schemas.master import MasterResponse, MasterCreate, MasterWithServices
 
 from app.database.connect import config
 from app.schemas.service import ServiceResponse, ServiceCreate
@@ -89,3 +90,135 @@ async def create_new_service(service_data: ServiceCreate, db: AsyncSession = Dep
     await db.refresh(new_service)
 
     return new_service
+
+
+@router.post(
+    "/masters/{master_id}/services/{service_id}",
+    response_model=MasterWithServices,
+    status_code=status.HTTP_200_OK,
+    summary="Добавить к мастеру услугу"
+)
+async def add_service_to_master(master_id: int, service_id: int, db: AsyncSession = Depends(config.get_db)):
+    master_request = await db.execute(
+        select(MasterModel).
+        where(MasterModel.id == master_id)
+    )
+    master = master_request.scalar_one_or_none()
+    if not master:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Мастер не существует"
+        )
+
+    service_request = await db.execute(
+        select(ServiceModel).
+        where(ServiceModel.id == service_id)
+    )
+    service = service_request.scalar_one_or_none()
+    if not service:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Услуга не существует"
+        )
+
+    if service in master.services:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Услуга уже есть у мастера"
+        )
+
+    master.services.append(service)
+
+    await db.commit()
+    await db.refresh(master)
+
+    return master
+
+
+@router.post(
+    "/appointments/masters/{master_id}/services/{service_id}/clients/{client_id}",
+    response_model=AppointmentResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Создать новую запись"
+)
+async def create_new_appointment(
+    appointment_data: AppointmentCreate,
+    master_id: int,
+    service_id: int,
+    client_id: int,
+    db: AsyncSession = Depends(config.get_db)
+):
+    appointment_request = await db.execute(
+        select(AppointmentModel).
+        where(
+            AppointmentModel.date == appointment_data.date,
+            AppointmentModel.start_time == appointment_data.start_time,
+            AppointmentModel.finish_time == appointment_data.finish_time,
+            AppointmentModel.price == appointment_data.price,
+            AppointmentModel.client_id == client_id,
+            AppointmentModel.master_id == master_id,
+            AppointmentModel.service_id == service_id,
+        )
+    )
+    appointment = appointment_request.scalar_one_or_none()
+    if appointment is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Запись уже существует"
+        )
+
+    master_request = await db.execute(
+        select(MasterModel).
+        where(MasterModel.id == master_id)
+    )
+    master = master_request.scalar_one_or_none()
+    if not master:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Мастер не существует"
+        )
+
+    service_request = await db.execute(
+        select(ServiceModel).
+        where(ServiceModel.id == service_id)
+    )
+    service = service_request.scalar_one_or_none()
+    if not service:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Услуга не существует"
+        )
+
+    result = await db.execute(
+        select(ClientModel)
+        .where(ClientModel.id == client_id)
+    )
+    client = result.scalar_one_or_none()
+    if not client:
+        raise HTTPException(
+            status_code=404,
+            detail="Клиент не найден"
+        )
+
+    new_appointment = AppointmentModel(
+        date=appointment_data.date,
+        start_time=appointment_data.start_time,
+        finish_time=appointment_data.finish_time,
+        price=appointment_data.price,
+        client_id=client_id,
+        master_id=master_id,
+        service_id=service_id,
+    )
+
+    db.add(new_appointment)
+    await db.commit()
+    await db.refresh(new_appointment)
+    return new_appointment
+
+
+
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.clients import router as clients_router
+from app.api.v1.admin import router as admin_router
+
+
 app = FastAPI(
     title="Beauty Salon API",
     version="1.0.0",
@@ -7,3 +10,4 @@ app = FastAPI(
 )
 
 app.include_router(clients_router, prefix="/api/v1")
+app.include_router(admin_router, prefix="/api/v1")
