b8ba7ae2d00c02d507c30622382ec16e6366a0a6
Platonn3 <platon.kurbatov87@gmail.com>
2025-12-02 01:53:04 +0500

    add main point

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from app.api.v1.clients import router as clients_router
 from app.api.v1.admin import router as admin_router
@@ -11,3 +12,7 @@ app = FastAPI(
 
 app.include_router(clients_router, prefix="/api/v1")
 app.include_router(admin_router, prefix="/api/v1")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
