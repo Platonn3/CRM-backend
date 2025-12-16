50c0ea061d98b6754e045fe60cf1352c16181ecd
Platonn3 <platon.kurbatov87@gmail.com>
2025-12-16 13:52:59 +0500

    add cors

--- app/main.py
+++ app/main.py
@@ -3,11 +3,21 @@ from fastapi import FastAPI
 from app.api.v1.clients import router as clients_router
 from app.api.v1.admin import router as admin_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Beauty Salon API",
     version="1.0.0",
     description="API для салона красоты"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(clients_router, prefix="/api/v1")
